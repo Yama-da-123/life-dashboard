8991406b65f361dbca161a1a2127088183789555
Yama-da-123 <ymd860421@gmail.com>
2026-05-12 21:34:17 +0900

    feat: add task page with jinja2

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World!"}
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/tasks")
+def task_page(request: Request):
+    tasks = [
+        "Python勉強",
+        "ガンプラ",
+        "買い物"
+    ]
+
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html", 
+        context={"tasks": tasks}
+    )
