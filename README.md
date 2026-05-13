6037a629a15bf6c476c96061b8df56d4431aea8c
Yama-da-123 <ymd860421@gmail.com>
2026-05-13 22:32:40 +0900

    feat: add task add

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # life-dashboard
 生活・趣味ダッシュボード開発プロジェクト
 
+## Sprint 0
+- FastAPI導入
+- root画面の表示
+- iPhoneでの表示
+
+## Sprint 1
+- task画面追加
+- Jinja2導入
+- iPhone確認
+---
+<br>
+現在進行中
+
+## Sprint 2
+- 入力欄表示
+- ボタン表示
+- タスク追加成功確認
+- iPhone確認
+
--- main.py
+++ main.py
@@ -1,20 +1,31 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+tasks = [
+            "Python勉強",
+            "ガンプラ",
+            "買い物"
+        ]
+
 @app.get("/tasks")
 def task_page(request: Request):
-    tasks = [
-        "Python勉強",
-        "ガンプラ",
-        "買い物"
-    ]
-
     return templates.TemplateResponse(
         request=request,
         name="index.html", 
         context={"tasks": tasks}
     )
+
+@app.post("/add")
+def add_task(request: Request, task: str = Form()):
+    tasks.append(task)
+
+    return templates.TemplateResponse(
+        request = request,
+        name="index.html",
+        context={"tasks": tasks}
+    )
+        
